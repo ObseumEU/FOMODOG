d04fad34fb328912bdba25f9dcd0ef5d9c16aec2
David Rosko <rosko.dav@gmail.com>
2023-06-20 23:20:59 +0200

    Remove old options

--- Options.cs
+++ Options.cs
@@ -1,9 +1,6 @@
 ﻿
 public class Options
 {
-    public string API_KEY { get; set; }
-    public string API_URL { get; set; }
     public string TELEGRAM_KEY { get; set; }
-    public string FOMODOG_DETAILS { get; set; }
     public string USER_PROMPT { get; set; }
 }
