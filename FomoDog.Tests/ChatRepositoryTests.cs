94254a9f81ae19014052038870cfcdbb12203a84
David Rosko <rosko.dav@gmail.com>
2023-10-18 11:54:52 +0200

    Test failed tests

--- FomoDog.Tests/ChatRepositoryTests.cs
+++ FomoDog.Tests/ChatRepositoryTests.cs
@@ -40,6 +40,7 @@ namespace FomoDog.Tests
 
         private static void ArrageDependencies(out MockFileSystem fileSystem, out IOptions<FileRepositoryOption> moqOptions)
         {
+            int ss = int.Parse("asdsad");
             fileSystem = new MockFileSystem();
             moqOptions = Options.Create(new FileRepositoryOption()
             {
